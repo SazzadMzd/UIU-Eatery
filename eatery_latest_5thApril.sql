3f4797b274670046741a89bff8e09093526eee6d
ShifaCleo <128978636+ShifaCleo@users.noreply.github.com>
2023-04-05 01:52:52 +0600

    modified db (item img)

---
+++ eatery_latest_5thApril.sql
@@ -0,0 +1,354 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2023 at 09:20 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `eatery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', '202cb962ac59075b964b07152d234b70'),
+(3, 'admin-user-2', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `customer_id` varchar(255) NOT NULL,
+  `user_type` varchar(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `base_price` int(255) NOT NULL,
+  `quantity` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `complaint`
+--
+
+CREATE TABLE `complaint` (
+  `id` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `user_id` varchar(255) NOT NULL,
+  `remarks` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `complaint`
+--
+
+INSERT INTO `complaint` (`id`, `type`, `user_id`, `remarks`) VALUES
+(2, 'student', 'student2', 'I have a complaint\r\n'),
+(3, 'faculty', 'faculty3', 'I have a complaint as a faculty'),
+(4, 'faculty', 'faculty2', 'i am giving second complaint...'),
+(5, 'faculty', 'faculty2', 'abcdef'),
+(6, 'faculty', 'faculty2', 'before update complaint'),
+(7, 'student', 'student2', 'ghgsgsghsdvg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `faculty_username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `room_no` int(10) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone_no` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `faculty`
+--
+
+INSERT INTO `faculty` (`faculty_username`, `password`, `room_no`, `email`, `phone_no`) VALUES
+('faculty', '1234', 0, '', 0),
+('faculty2', '81dc9bdb52d04dc20036dbd8313ed055', 232, 'faculty2@gmail.com', 192222222),
+('faculty3', '81dc9bdb52d04dc20036dbd8313ed055', 232, 'aaaa', 123);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `restaurant_id` int(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `stock` int(255) NOT NULL,
+  `price` int(255) NOT NULL,
+  `image` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`restaurant_id`, `item_name`, `stock`, `price`, `image`) VALUES
+(17, 'Beef Burger', 145, 375, '../images/food_items/OIP (1).jpg'),
+(17, 'Chicken Burger', 15, 75, '../images/restuarant_image/rest6eatery.jpg'),
+(17, 'Meat Box', 33, 150, '../images/food_items/OIP (2).jpg'),
+(17, 'Pasta', 50, 125, '../images/food_items/pasta.jpg'),
+(17, 'Singara', 12, 5, '../images/food_items/singara.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` varchar(255) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `order_status` int(11) NOT NULL DEFAULT 0 COMMENT '0=pending | 1=confirm',
+  `restaurant_name` varchar(255) NOT NULL,
+  `user_type` varchar(255) NOT NULL,
+  `payment_method` varchar(255) NOT NULL,
+  `receiving_method` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `customer_id`, `order_date`, `order_status`, `restaurant_name`, `user_type`, `payment_method`, `receiving_method`) VALUES
+(1, 'student2', '2023-04-04 17:42:29', 1, 'khans kitchen', 'student', 'COD', 'PICKUP'),
+(2, 'student2', '2023-04-04 17:42:26', 1, 'khans kitchen', 'student', 'COD', 'PICKUP'),
+(3, 'student2', '2023-04-04 17:42:23', 1, 'khans kitchen', 'student', 'COD', 'PICKUP');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_details`
+--
+
+CREATE TABLE `order_details` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `base_amount` varchar(255) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `total_price` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order_details`
+--
+
+INSERT INTO `order_details` (`id`, `order_id`, `item_name`, `base_amount`, `quantity`, `total_price`) VALUES
+(1, 1, 'Chicken Burger', '75', '1', '75'),
+(2, 1, 'Singara', '5', '4', '20'),
+(3, 2, 'Singara', '5', '15', '60'),
+(4, 3, 'Chicken Burger', '75', '1', '75'),
+(5, 3, 'Singara', '5', '4', '20'),
+(6, 3, 'Pasta', '125', '1', '125');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `owner`
+--
+
+CREATE TABLE `owner` (
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `restaurant_id` int(255) NOT NULL,
+  `status` varchar(255) DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `owner`
+--
+
+INSERT INTO `owner` (`email`, `password`, `restaurant_id`, `status`) VALUES
+('3@gmail.com', '202cb962ac59075b964b07152d234b70', 23, 'accepted'),
+('aaa@gmail.com', '202cb962ac59075b964b07152d234b70', 24, 'pending'),
+('abc@gmail.com', '202cb962ac59075b964b07152d234b70', 18, 'accepted'),
+('bikrampur@gmail.com', '202cb962ac59075b964b07152d234b70', 19, 'accepted'),
+('khans@gmail.com', '202cb962ac59075b964b07152d234b70', 17, 'accepted'),
+('sad@gmail.com', '202cb962ac59075b964b07152d234b70', 21, 'accepted');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restaurant`
+--
+
+CREATE TABLE `restaurant` (
+  `restaurant_id` int(255) NOT NULL,
+  `restaurant_name` varchar(255) NOT NULL,
+  `img_dir` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `restaurant`
+--
+
+INSERT INTO `restaurant` (`restaurant_id`, `restaurant_name`, `img_dir`) VALUES
+(17, 'khans kitchen', '../images/restuarant_image/rest6eatery.jpg'),
+(18, 'ABC', '../images/restuarant_image/rest4eatery.jpg'),
+(19, 'Bikrampurs Khan', '../images/restuarant_image/foodpic.png'),
+(21, 'SAD rest', '../images/restuarant_image/rest3eatery.jpg'),
+(23, '3', '../images/restuarant_image/rest6eatery.jpg'),
+(24, 'aaa', '../images/restuarant_image/rest6eatery.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `student_id` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone_no` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`student_id`, `password`, `email`, `phone_no`) VALUES
+('', 'd41d8cd98f00b204e9800998ecf8427e', '', 0),
+('11122', '81dc9bdb52d04dc20036dbd8313ed055', '1111', 222),
+('student', '1234', '', 0),
+('student2', '81dc9bdb52d04dc20036dbd8313ed055', 'st2@gmail.com', 1833333333),
+('student3', '81dc9bdb52d04dc20036dbd8313ed055', '3@ggg', 123);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `complaint`
+--
+ALTER TABLE `complaint`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`faculty_username`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`restaurant_id`,`item_name`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_details`
+--
+ALTER TABLE `order_details`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `owner`
+--
+ALTER TABLE `owner`
+  ADD PRIMARY KEY (`email`),
+  ADD KEY `restaurant_id` (`restaurant_id`);
+
+--
+-- Indexes for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  ADD PRIMARY KEY (`restaurant_id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `complaint`
+--
+ALTER TABLE `complaint`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `order_details`
+--
+ALTER TABLE `order_details`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `restaurant`
+--
+ALTER TABLE `restaurant`
+  MODIFY `restaurant_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
